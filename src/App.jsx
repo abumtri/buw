7e187dbdf4c3634f6775eeeb6b5d7cb19c2437ac
abumtri <abdulloxv.31@gmail.com>
2025-07-21 15:03:29 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>Welcome to REACT JS</h1>
+        <h2>this is my first react app</h2>
+        <ul className='li-wrap'>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>React</li>
+        </ul>
+        <h3>What is react</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias placeat quidem adipisci omnis voluptates suscipit nihil. Nobis voluptatibus error fugit porro sint quas ducimus nulla laboriosam doloremque illum repellat, quibusdam iusto accusantium est atque natus molestias tenetur. Quam tenetur veniam cum harum laudantium delectus, autem quaerat? Similique nulla animi incidunt?</p>
+        <h3>What is single-page aplication(SPA)</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere distinctio sequi ut itaque consequuntur temporibus obcaecati tenetur minima voluptas non commodi eum, aliquam deleniti repellat ex maiores id excepturi iusto exercitationem recusandae, officia aliquid qui nemo voluptatibus? Reprehenderit nesciunt nemo, ab beatae fugiat cupiditate facilis accusamus tempora cumque asperiores necessitatibus accusantium!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
